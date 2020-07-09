9ea5a438552d1745c6579138d4e465792822b87d
Cristian Ortega <emanuel.ortega@hotmail.com>
2020-07-08 22:25:00 -0500

    Fixed app

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -15,8 +15,13 @@
         //var_dump($result);
         if (!empty($user_email) || !empty($user_password) || !empty($user_name))
         {
-        $sqlquery = "INSERT INTO(name,email,password) users VALUES($user_name,$user_email,$user_password)";
-        echo "Thanks for Registering";
+        $sqlquery = "INSERT INTO users(name,email,password) VALUES('$user_name','$user_email',$user_password)";
+        if(mysqli_query($connection,$sqlquery)){
+            echo "Thanks for Registering";
+        }
+        else{
+            echo "Error: " . $sqlquery. "<br>" . mysqli_error($connection);
+        }
         }
         else {
             echo "Sorry bro, you're missing either an email or password";
